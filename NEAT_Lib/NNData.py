1dfc58356fde7c952d647c7d176d4347410f23dd
Jacob Maxson <46852825+Rocksrock18@users.noreply.github.com>
2021-01-31 05:46:17 -0500

    Update NNData.py

--- NEAT_Lib/NNData.py
+++ NEAT_Lib/NNData.py
@@ -1,8 +1,8 @@
 class NNData(object):
     def __init__(self):
-        self.struct_scale = 2.5
+        self.struct_scale = 6
         self.weight_scale = 2
-        self.diff_threshold = 3
+        self.diff_threshold = 1.8
         global next_innov
         next_innov = 1
 
