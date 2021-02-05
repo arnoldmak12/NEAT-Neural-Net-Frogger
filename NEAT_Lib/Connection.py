35754f8f3eefd97f40497fbec51a6b7820ecc7db
Jacob Maxson <46852825+Rocksrock18@users.noreply.github.com>
2021-02-05 15:57:07 -0500

    Update Connection.py

--- NEAT_Lib/Connection.py
+++ NEAT_Lib/Connection.py
@@ -8,4 +8,7 @@ class Connection(object):
         self.enabled = True
         self.innov = innov
 
+    def __str__(self):
+        return "Input: "+ str(self.input_id) + ", Output: " + str(self.output_id) + ", Weight: " + str(self.weight) + ", Enabled: " + str(self.enabled) + ", Innov: " + str(self.innov)
+
 
