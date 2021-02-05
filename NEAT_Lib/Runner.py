d729035c6fe2b379003ffcaf39470287e54a7c2e
Jacob Maxson <46852825+Rocksrock18@users.noreply.github.com>
2021-02-05 15:56:12 -0500

    Update Runner.py

--- NEAT_Lib/Runner.py
+++ NEAT_Lib/Runner.py
@@ -2,73 +2,49 @@ import numpy as np
 from NeuralNet import NeuralNet
 from Population import Population
 from NNData import NNData
-
-# This demo shows how to use the NeuralNet library
-# In this example, we give the NN 2 random floats, and train it to determine which float is greater than the other.
-
-
-# This is how we will generate data for the NN to train with. Format: [float1, float2, expected_output]
-def generate_sample_data(size):
-    data = np.random.randn(size,3)
-    for i in range(size):
-        data[i][2] = (1,0) [data[i][0] < data[i][1]]
-    return data
-
-
-pop = Population(5, 5, 500, NNData())
-
-pop.simulate_generations(100)
-
-# Next, you need to generate the starting NN. Storing the result is optional.
+from Frogger import Frogger
 
 
 
-print("\nTraining in progress...\n")
 
-# Here we specify some key parameters that affect how the NN trains.
-num_batches = 1 # 5000
-batch_size = 1 # 20
-training_rate = 0.05
+print("\n\n===================================")
+print("\tWelcome to Frog.io!")
+print("===================================\n")
 
-# This is optional, but gives us an idea of how accurate the NN is.
-num_correct = 0
-num_total = 0
+print("-----------------------------------\n       Simulation Parameters\n-----------------------------------\n")
 
-# This is where we train the NN.
-for i in range(num_batches):
-    # First, we retrieve the data for a given batch.
-    sample_data = generate_sample_data(batch_size)
-    # Then we get a prediction for each data set in the batch.
-    for k in range(batch_size):
-        pass
-        # To start, we pass the inputs to the NN and get the output nodes.
-        #outputs = net.forward_prop([sample_data[k][0], sample_data[k][1]])
-        # Next, we need to determine what the expected output should be based on the inputs.
-        #expected_outputs = [sample_data[k][2]]
-        # We compare the output nodes to the expected output nodes and determine the cost of each node.
-        #costs = net.calculate_costs(outputs, expected_outputs)
-        # Finally, we use the costs to adjust the weights of the NN.
-        #net.backward_prop(costs, training_rate)
-        # Here we can update the parameters that determine the NN's accuracy.
-        #num_total = num_total+1
-        #num_correct = (num_correct,num_correct+1) [np.abs(outputs[0]-expected_outputs[0]) < 0.5]
-    #print(str(i+1) + "/" + str(num_batches) + " batches completed, Accuracy: " + str(round(num_correct/num_total*100, 3)) + "%\t", end='\r')
+pop_size = input(" - How many frogs would you like in each generation? (Recommended: 200-400) ")
+static_start = input(" - Do you want the cars to start in the same position each generation? (Y/N) ")
+is_static = static_start == "Y" or static_start == "y"
+num_generations = input(" - How many generations would you like to simulate? (Recommended: 30-50) ")
+print_best = input(" - Do you want to print the best network after each generation? (Y/N) ")
+is_print = print_best == "Y" or print_best == "y"
 
-print("\n\n", end='\n')
-#pop.simulate_generations(100)
-print("Ending Neural Net:\n")
-#pop.print_pop()
-input("Finished")
+print("\n\n-----------------------------------\n        General Information\n-----------------------------------\n")
 
-# Now we can feed custom inputs to the NN and see how accurate it is. Note: inputs for NN should be scaled to be within [-5,5]
-while True:
-    first = input("\nFirst Number: ")
-    second = input("Second Number: ")
-    test_data = [float(first), float(second)]
-    outputs = net.forward_prop(test_data)
-    print(outputs)
-    if outputs[0] > 0.5:
-        print("First number is greater than the second!")
-    else:
-        print("Second number is greater than the first!")
+print("\t- A neural network is saved and updated for you automatically after each generation.\n\t- If you've run this simulation before, it'll be included in the starting population.")
+input("\nPress [Enter] when you are ready to begin the simulation")
+
+print("\n\n-----------------------------------\n         Simulation Start\n-----------------------------------\n")
+
+print("Preparing the starting population. This will only take a moment...")
+
+data = NNData()
+pop = Population(13, 5, int(pop_size), data)
+
+print("Starting population complete!")
+
+print("\n\n===================================\n    Simulating " + num_generations + " generation(s)\n===================================\n\n")
+
+pop.simulate_generations(int(num_generations), is_static, is_print)
+
+print("\n-----------------------------------\n          Continuing play\n-----------------------------------\n")
+
+response = input("\n\nWould you like to play continuous frogger games with the best frog? (Y/N) ")
+if response == "Y" or response == "y":
+    while True:
+        frogger = Frogger()
+        frog = Frogger.Frog(350,750,50, pop.get_master_parent(13, 5, data), frogger.game_data)
+        frogger.begin_game([frog], is_static)
+
 
